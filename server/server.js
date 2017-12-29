952c2e13af5504fddbc2be1f836e8d97ce4017c8
Collin Cavallo <collincavallo@gmail.com>
2017-12-29 13:42:24 -0600

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
-var {ObjectId} = require('mongodb');
+const {ObjectId} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -59,8 +60,33 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
+app.patch('/todos/:id' ,(req, res) => {
+    var id = req.params.id;
+
+    var body = _.pick(req.body, ['completed']);
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send()
+    })
+});
+
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server is listening  on port ${port}`)
 });
 
 
