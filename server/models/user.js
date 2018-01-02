c3fd0819c9da5f3526f1334414d26c87b6028101
Collin Cavallo <collincavallo@gmail.com>
2018-01-02 10:54:06 -0600

    Added POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -66,6 +66,27 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) =>{
+            bcrypt.compare(password , user.password, (err, res) =>{
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                };
+                
+            });
+        });
+
+    });
+};
 UserSchema.pre('save', function(next) {
     var user = this;
     
@@ -79,7 +100,10 @@ UserSchema.pre('save', function(next) {
    } else {
        next();
    }
-})
+});
+
+
+
 
 var Users = mongoose.model('Users', UserSchema)
 
